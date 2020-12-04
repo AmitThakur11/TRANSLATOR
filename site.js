d6bd125fd336466f23a77cef3474a6cfb7cf3c40
AmitThakur11 <amithakur081@gmail.com>
2020-12-04 21:32:47 +0530

    output area added and styled

--- site.js
+++ site.js
@@ -1,9 +1,12 @@
 var trans_btn = document.querySelector("#primary_btn");
 var text = document.querySelector("#input_text");
+var output = document.querySelector("#output")
+
+
 
 function btn_event(){
-    console.log("input", text.value);
-    
+    output.innerText = text.value;
+
 };
 
 trans_btn.addEventListener("click", btn_event);
